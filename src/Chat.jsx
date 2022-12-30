696a6189da3af66779fc81dc93da7976503076eb
Mantosh <kyamantoshkumar@gmail.com>
2022-12-30 09:39:33 +0530

    Update Chat.jsx

--- src/Chat.jsx
+++ src/Chat.jsx
@@ -2,80 +2,53 @@ import React, { useEffect, useRef, useState } from "react";
 import ScrollToBottom from "react-scroll-to-bottom";
 import Popup from 'reactjs-popup';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-const TableRows = ({ rows, tableRowRemove, onValUpdate, tableFormSubmit, onFormSubmit }) => { 
-  return rows?.map((rowsData, index ) => { 
-     const { id, name, profile } = rowsData;
-     return (
+
+const TableRows = ({ rows, tableRowRemove, onValUpdate, tableRowSubmit }) => {
+
+  return rows?.map((rowsData, index) => {
+    const { id, name, profile } = rowsData;
+    return (
       <tr key={index}>
-        <td> 
-          <input 
-           type="text"
-           value={id}
-           onChange={(event) => onValUpdate(index,event)}
-           name="id"
-           className="form-control"
-           />
-        </td>
         <td>
-          <input  
-           type="text"
-           value={name}
-           onChange={(event) => onValUpdate(index, event)}
-           name="name"
-           className="form-control"
+          <input
+            type="text"
+            value={id}
+            onChange={(event) => onValUpdate(index, event)}
+            name="id"
+            className="form-control"
           />
         </td>
         <td>
-          <input 
-           type='text'
-           value={profile}
-           onChange={(event) => onValUpdate(index, event)}
-           name="profile"
-           className="form-control"
+          <input
+            type="text"
+            value={name}
+            onChange={(event) => onValUpdate(index, event)}
+            name="name"
+            className="form-control"
           />
         </td>
-        <button className="btn btn-danger" onClick={() => tableRowRemove(index)}>
-          Delete Row
+        <td>
+          <input
+            type='text'
+            value={profile}
+            onChange={(event) => onValUpdate(index, event)}
+            name="profile"
+            className="form-control"
+          />
+        </td>
+        <button className="btn text-white" style={{background:'#fcd2d2'}} onClick={() => tableRowRemove(index)}>
+          Remove
         </button>
-        {/* <div className="d-grid">
-        <button
-          type="submit"
-          onClick={() => tableFormSubmit(data)}
-          className="btn btn-success"
-        >submit</button>
-      </div> */}
+        <td>
+      <div className="" onClick={() => tableRowSubmit(index)} />
+        </td>
       </tr>
-     )
+
+    )
   })
 }
 
 
-// const data = ({value, onValUpdate  }) => {
-//    return value.map((rowsData, index ) => {
-//     const { data } = rowsData
-//     return (
-      
-//       <tr onValUpdate={data}>{onValUpdate.rowsData}</tr>
-//     )
-//    })
-// } 
-
-// const memberData = [
-//   {
-//     id: 2123,
-//     Name: "Subham"
-//   },
-
-//   {
-//     id:2234,
-//     Name:'Saurabh'
-//   },
-
-//   {
-//     id:1123,
-//     Name:'Shreyas'
-//   },
-// ];
 
 function Chat({ socket, username, room, data, placeholder }) {
   const [currentMessage, setCurrentMessage] = useState("");
@@ -84,6 +57,7 @@ function Chat({ socket, username, room, data, placeholder }) {
   const onFileChange = event => {
     setImage({ selectedFile: event.target.files[0] });
   };
+  const [datavalues, setDataValues] = useState();
 
   const sendMessage = async () => {
     if (currentMessage !== " " || image !== 0) {
@@ -106,7 +80,6 @@ function Chat({ socket, username, room, data, placeholder }) {
   };
 
   const [text, setText] = useState("");
-  // const [isFound, setFound] = useState(false);
   const paragraphEl = useRef(null)
 
   const handleInput = (e) => {
@@ -120,7 +93,7 @@ function Chat({ socket, username, room, data, placeholder }) {
       ' Yes! I Linked the Device. This device are "Liked from Frontend to Backend" '
     )
   }
- 
+
   const [rows, initRows] = useState([])
   const addRowTable = () => {
     const data = {
@@ -130,20 +103,29 @@ function Chat({ socket, username, room, data, placeholder }) {
     };
     initRows([...rows, data])
   }
-   const tableRowRemove = (index) => {
+  const tableRowRemove = (index) => {
     const dataRow = [...rows];
     dataRow.splice(index, 1)
     initRows(dataRow)
-   }
+  }
 
-   const onValUpdate = (i, event) => {
-    const  {name, value} = event.target;
+const tableRowSubmit = (index) => {
+  const dataRow = [...rows];
+  dataRow.slice(index, 1)
+  initRows(dataRow)
+
+  setDataValues(dataRow);
+  console.log(dataRow);
+}
+
+  const onValUpdate = (i, event) => {
+    const { name, value } = event.target;
     const data = [...rows];
     data[i][name] = value;
     initRows(data)
     console.log(data)
   };
- 
+
   const onValChange = (event) => {
     const data = (res) => ({
       ...res,
@@ -152,39 +134,6 @@ function Chat({ socket, username, room, data, placeholder }) {
     addRowTable(data);
   };
 
-  const onFormSubmit = (event) => {
-    //  event.preventDefault();
-    const checkVal = !Object.values(rows).every((res) => res === "");
-    if (checkVal) {
-      const dataObj = (data) => [...data, rows];
-       TableRows(dataObj);
-      const isEmpty = { id: "", name: "", profile: "" };
-     TableRows(isEmpty);
-    }
-    console.log(onFormSubmit);
-  };
-
-  // const [tableData, setTableData] = useState([]);
-  const [id, setId] = useState('');
-  const [name, setName] = useState('');
-  const [profile, setProfile] = useState('');
-  
-  const transferValue = (event) => {
-    event.preventDefault();
-    const val = {
-      id:'',
-      name:'',
-      profile:'',
-    };
-    //  props.func(val);
-    clearState();
-  };
-  const clearState = () => {
-    setId('');
-    setName('');
-    setProfile('');
-  };
-  
 
   useEffect(() => {
     socket.on("receive_message", (data) => {
@@ -194,7 +143,7 @@ function Chat({ socket, username, room, data, placeholder }) {
 
   const myStyle = {
     fontFamily: 'Text New Roman',
-    background:'#d1e9f5'
+    background: '#f8fcfd'
   }
 
   return (
@@ -205,14 +154,13 @@ function Chat({ socket, username, room, data, placeholder }) {
         <div>
 
           <input
-            style={ myStyle }
-            className="placeholder color-light bg-transparent placeholder-white"
+            style={myStyle}
+            className="placeholder text-light bg-transparent placeholder-light opacity-3"
             type="text"
             placeholder="Search... "
             value={text}
             onChange={handleInput}
           />
-          {/* <i className="fa-solid fa-magnifying-glass"></i> */}
           <p className="text-black bg-primary" ref=
             {paragraphEl}
           >
@@ -223,9 +171,9 @@ function Chat({ socket, username, room, data, placeholder }) {
           </button>}
           position="left top" className="bg-primary"
         >
-          <div style={myStyle} className="btn-light text-start rounded-1" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Left popover">
+          <div style={myStyle} className=" btn-light rounded-1" data-bs-container="body" data-bs-toggle="popover" data-bs-placement="left" data-bs-content="Left popover">
 
-            <button type="button" className="btn btncli" data-bs-toggle="modal" data-bs-target="#exampleModal">New Member</button> 
+            <button type="button" className="btn btncli" data-bs-toggle="modal" data-bs-target="#exampleModal">New Member</button>
 
             <div className="modal " id="exampleModal" aria-labelledby="exampleModalLabel" aria-hidden="true">
               <div className="modal-dialog">
@@ -253,63 +201,67 @@ function Chat({ socket, username, room, data, placeholder }) {
                 </div>
               </div>
             </div>
-            <div><h6 onClick={onCreateTable} className="btn btncl">Linked Device</h6>
-            <div className="table-responsive">
-  <table className="table table-bordered table-hover" style={{width: '80%'}}>
-    <thead>
-      <tr>
-        <th>ID</th>
-        <th>Name</th>
-        <th>Profile</th>
-      </tr>
-    </thead>
-    <tbody>
-      {
-        rows.map((props) => {
-<tr ng-repeat="item in contacts">
-        <td>{rows.id}</td>
-        <td>{rows.name}</td>
-        <td>{rows.profile}</td>
-      </tr>
-        })
-      } 
-      
-    </tbody>
-  </table>
- </div>
-</div>
+
+            <div className="w-75"><h6 onClick={onCreateTable} className="btn btncl">Linked Device</h6>
+              <div className="table-responsive">
+                <table className="table table-bordered table-hover" style={{  }}>
+                  <thead>
+                    <tr>
+                      <th>ID</th>
+                      <th>Name</th>
+                      <th>Profile</th>
+                    </tr>
+                  </thead>
+                  <tbody onClick={() => tableRowSubmit(rows)}>
+                    {
+                      datavalues?.map(( rows) => (
+                        // console.log(rows);
+                        <tr ng-repeat="item in contacts">
+                          <td>{rows.id}</td>
+                          <td>{rows.name}</td>
+                          <td>{rows.profile}</td>
+                        </tr>
+                      ))
+                    }
+
+                  </tbody>
+                </table>
+              </div>
+            </div>
             <div><h6 className="btn btncl">Started Message</h6>
-                <div>
-                  <h5 className="text-center">Group Memeber</h5>
+              <div className="w-75 text-end">
+                <h5 className="text-center">Group Memeber</h5>
                 <table>
-                  <thead className="col d-flex">
-                    <tr className="col d-flex bg-info">
+                  <thead className="col d-flex mx-2">
+                    <tr className="col d-flex">
                       <th className="col">Id</th>
                       <th className="col">Name</th>
                       <th className="col">Profile</th>
-                      </tr>
-                      <tr>
-                      <th>
-                    <button className="btn btn-secondary" onClick={addRowTable}>Insert Row</button>
-                    </th>
+                    </tr>
+                    <tr>
+                      <th className="me-5 ">
+                        <button className="btn btn-secondary mx-2  h-75 w-100" onClick={addRowTable}>Insert </button>
+                      </th>
                     </tr>
                   </thead>
-              <tbody>
-                <tbody>
-                  <TableRows 
-                   rows={rows}
-                   tableRowRemove={tableRowRemove}
-                   onValUpdate={onValUpdate}
-                   onValChange={onValChange}
-                  />
-                </tbody>
-                <tr className="text-end">
-                <button className="btn btn-warning text-center w-100" onClick={() => onFormSubmit(rows)}>Save Data</button>
-                </tr>
-              </tbody>
+                  <tbody>
+                    <tbody>
+                      <TableRows
+                        rows={rows}
+                        tableRowRemove={tableRowRemove}
+                        onValUpdate={onValUpdate}
+                        onValChange={onValChange}
+                        tableRowSubmit={tableRowSubmit}
+                      />
+                    </tbody>
+                    <tr className="text-end">
+                      <button style={{background:'#c8f3d8'}} className="btn text-center w-100" onClick={() => tableRowSubmit(rows)}> Save </button>
+                    </tr>
+                  </tbody>
                 </table>
-                </div>
+              </div>
             </div>
+
             <div><h6 className="btn btncl">Setting</h6></div>
           </div>
         </Popup>
@@ -378,56 +330,3 @@ function Chat({ socket, username, room, data, placeholder }) {
 }
 
 export default Chat;
-
- 
-/*
-
-import React, { useState, useRef } from "react";
-
-export default function App() {
-  const [text, setText] = useState("");
-  const [isFound, setFound] = useState(false);
-  const paragraphEl = useRef(null);
-
-  const handleInput = (e) => {
-    setText(e.target.value);
-    const paragraphText = paragraphEl.current.textContent;
-    if (e.target.value !== "" && paragraphText.includes(e.target.value)) {
-      setFound(true);
-    } else {
-      setFound(false);
-    }
-  };
-
-  return (
-    <div>
-      // <h1>Search text</h1>
-      <input
-        type="text"
-        placeholder="Type here"
-        value={text}
-        onChange={handleInput}
-      />
-      <p ref={paragraphEl}>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-        veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-        commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-        velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint
-        occaecat cupidatat non proident, sunt in culpa qui officia deserunt
-        mollit anim id est laborum.
-      </p>
-      {isFound && <p style={{ color: "green" }}>We found it</p>}
-    </div>
-  );
-}
-     3
-*/
-
-/* 
-
-
-
-   
-    {/* <button className="search placeholder-light" style={{width:'60px', background:'transparent', border:'none'}}><input  className="placeholder placeholder-white text-light bg-dark" type="search" placeholder="search..." onChange={handleChange}/></button> 
-/> */
