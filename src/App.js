7536290a314ca84eb1d8cc50411aaaf177650cee
Mantosh <kyamantoshkumar@gmail.com>
2023-01-01 18:24:15 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,56 +1,21 @@
-
+import {Routes, Route, BrowserRouter } from "react-router-dom"
 import "./App.css";
-import io from "socket.io-client";
-import { useState } from "react";
-import Chat from "./Chat";
-// import BookData from "../Data.json"
-// import SearchBar from "./Components/SearchBar";
+import Login from "./pages/Login";
+import Registration from "./pages/Registration";
+import Room from "./pages/Room";
 
-const socket = io.connect("http://localhost:4000");
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [room, setRoom] = useState("");
-  const [showChat, setShowChat] = useState(false);
-
-  const joinRoom = () => {
-    if (username !== "" && room !== "") {
-      socket.emit("join_room", room);
-      setShowChat(true);
-    }
-  };
-  
-  // const onButtonClick = () => {
-  // `current` points to the mounted file input element
-  //  inputFile.current.click();
-  // };
-
   return (
     <div className="App">
-      {!showChat ? (
-        <div className="joinChatContainer">
-          <h3>Join A Chat</h3>
-          <input
-            type="text"
-            placeholder="John..."
-            onChange={(event) => {
-              setUsername(event.target.value);
-            }}
-          />
+      <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/registration" element={<Registration />} />
+        <Route path="/room" element={<Room />} />
+      </Routes>
+      </BrowserRouter>
 
-          <input
-            type="text"
-            placeholder="Room ID..."
-            onChange={(event) => {
-              setRoom(event.target.value);
-            }}
-          />
-          <button onClick={joinRoom}>Join A Room</button>
-        </div>
-      ) : (
-        <Chat socket={socket} username={username} room={room} />
-      )}
-     
     </div>
   );
 }
