2b1f95f9bad49e4b53ccafd70b0dcdd7a5d2a8ff
Mantosh <kyamantoshkumar@gmail.com>
2023-01-02 15:36:42 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
                 style={{
                     display: error ? '' : 'none',
                 }}>
-                <h6 className='bg-danger text-light py-2 px-3 rounded fw-bold'>Please enter all the fields</h6>
+                <h6 style={{background:'#f79d9d'}} className='text-danger text-center py-2 px-3 rounded fw-bold'>Please enter all the fields</h6>
             </div>
         );
     };
@@ -60,7 +60,7 @@ const Login = () => {
                 style={{
                     display: submitted ? '' : 'none',
                 }}>
-                <h6 className='bg-success text-light py-2 px-3 rounded fw-bold'>Login successfully!!</h6>
+                <h6 style={{background:'#5fff8f'}} className='text-success text-center py-2 px-3 rounded fw-bold'>Login successfully!!</h6>
             </div>
         );
     };
@@ -71,55 +71,58 @@ const Login = () => {
         body: "center",
         // fontFamily: 'text new roman'
     }
+
+    const myFont = {
+     fontFamily: 'serif'
+    }
     return (
         <>
             <div className="conatiner">
                 <div className="col-md-8 col-sm-12">
                     {/* 1 of 8 */}
                 </div>
-                <div className="col-md-4 col-sm-12">
-                    <div className="col-md-12 col-sm-12 form-control">
+                <div className="col-md-4 col-sm-12 mx-auto w-75 px-5">
+                    <div className="col-md-12 col-sm-12">
                         <div className="row">
-                       
                             <div className="messages">
                                 {errorMessage()}
                                 {successMessage()}
                             </div>
-                                <h6 className="fs-3 bg-primary fw-bold ff-timeeNewRoman text-light text-center py-2 rounded">Login Page</h6>
-                                <form className="form-control w-100">
-                                    <table>
-                                        <thead>
-                                            <th>
-                                                <tr>
-                                                    <label className="fs-3" htmlFor="user">User Email</label>
-                                                    <input
-                                                        style={myStyle}
-                                                        className="input form-control"
-                                                        type="email"
-                                                        value={email}
-                                                        placeholder="Enter User..."
-                                                        onChange={handleEmail} />
-                                                </tr>
-                                                <tr>
-                                                    <label className="fs-3" htmlFor="email">User Passowrd</label>
-                                                    <input
-                                                        style={myStyle}
-                                                        className="input form-control"
-                                                        type="password"
-                                                        value={password}
-                                                        placeholder="Enter password..."
-                                                        onChange={handlePassword} />
-                                                    {/* <input type="text" name="passw" id="passw" value={email} onChange={(e)=>setEmail(e.target.value)}/>  */}
-                                                </tr>
-                                                <tr>
-                                                    <button className="btn btn-success my-3 mw-100" onClick={handleSubmit}>Login</button>
-                                                    <button className="btn btn-primary mx-3" onClick={()=>navigate("/registration")}>Registration</button>
-                                                </tr>
-                                            </th>
-                                        </thead>
-                                        
-                                    </table>
-                                    </form>
+                            <h6 style={{background:'#b078fa'}} className="fs-3 fw-bold text-light text-center py-2 rounded">Login Page</h6>
+                            <form className="form-control w-100 px-4" style={{background:'#c1d8fc'}}>
+
+                                <table className="ms-4">
+                                    <thead>
+                                        <th>
+                                            <tr>
+                                                <label style={myFont} className="fs-3" htmlFor="user">User Email</label>
+                                                <input
+                                                    style={myStyle}
+                                                    className="input form-control"
+                                                    type="email"
+                                                    value={email}
+                                                    placeholder="Enter User..."
+                                                    onChange={handleEmail} />
+                                            </tr>
+                                            <tr>
+                                                <label style={myFont} className="fs-3 mt-2" htmlFor="email">User Passowrd</label>
+                                                <input
+                                                    style={myStyle}
+                                                    className="input form-control"
+                                                    type="password"
+                                                    value={password}
+                                                    placeholder="Enter password..."
+                                                    onChange={handlePassword} />
+                                                {/* <input type="text" name="passw" id="passw" value={email} onChange={(e)=>setEmail(e.target.value)}/>  */}
+                                            </tr>
+                                            <tr>
+                                                <button style={{background:'#7afcb0'}} className="btn mt-3 my-1 fs-6 w-100 text-success" onClick={handleSubmit}>Login</button>
+                                                <button style={{background:'#91b9f5'}}className="btn text-primary fs-6 mb-3  w-100" onClick={() => navigate("/registration")}>Registration</button>
+                                            </tr>
+                                        </th>
+                                    </thead>
+                                </table>
+                            </form>
                         </div>
                     </div>
                 </div>
