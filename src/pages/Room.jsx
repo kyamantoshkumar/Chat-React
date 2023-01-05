178ac98add332413102432e4c0c5763ac58f8f06
Mantosh <kyamantoshkumar@gmail.com>
2023-01-05 09:33:52 +0530

    Update Room.jsx

--- src/pages/Room.jsx
+++ src/pages/Room.jsx
@@ -15,11 +15,12 @@ const Room = () => {
   };
 
   return (
-    <div>  {!showChat ? (
+    <div className=''>  {!showChat ? (
         <div className="joinChatContainer">
           <h3>Join A Chat</h3>
           <input
-            type="text"
+           className='w-100'
+            type="input"
             placeholder="John..."
             onChange={(event) => {
               setUsername(event.target.value);
@@ -27,13 +28,14 @@ const Room = () => {
           />
 
           <input
+          className='w-100'
             type="text"
             placeholder="Room ID..."
             onChange={(event) => {
               setRoom(event.target.value);
             }}
           />
-          <button onClick={joinRoom}>Join A Room</button>
+          <button className='w-100' onClick={joinRoom}>Join A Room</button>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
