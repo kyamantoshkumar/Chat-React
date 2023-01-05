b834fff9cf21abee82eb6503d416336f4681031c
Mantosh <kyamantoshkumar@gmail.com>
2023-01-05 09:33:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,8 +3,8 @@ import "./App.css";
 import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Room from "./pages/Room";
+import Counter from "./Search/effect";
 import GeeksforGeeks from "./Search/toast";
-
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
         <Route path="/registration" element={<Registration />} />
         <Route path="/room" element={<Room />} />
         <Route path="/geeksforgeeks" element={<GeeksforGeeks/>} />
+        <Route path ="/counter" element={<Counter/>} />
       </Routes>
       </BrowserRouter>
     </div>
