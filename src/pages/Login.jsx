7a629925fda7baf34df913aaaca4047678525848
Mantosh <kyamantoshkumar@gmail.com>
2023-01-06 10:27:21 +0530

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,133 +1,188 @@
-
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { ToastContainer, toast } from "react-toastify";
-import Regestration from "../pages/Registration"
-import "../assets/css/login.css"
+import Regestration from "../pages/Registration";
+import "../assets/css/login.css";
 const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPasssword] = useState("");
 
-    const navigate = useNavigate()
-    const [email, setEmail] = useState("")
-    const [password, setPasssword] = useState("")
+  // State for checking the error
+  const [submitted, setSubmitted] = useState(false);
+  const [error, setError] = useState(false);
+  // use toast notify message
+  const notify = () => {
+    toast("");
+  };
 
-    // State for checking the error
-    const [submitted, setSubmitted] = useState(false)
-    const [error, setError] = useState(false)
-    // use toast notify message
-    const notify = () => {
-        toast("");
+  // Handeling the Email change
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handeling the Password Change
+  const handlePassword = (e) => {
+    setPasssword(e.target.value);
+    setSubmitted(false);
+  };
+
+  // Handel Submit is login
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      setError(true);
+    } else {
+      setSubmitted(true);
+      setError(false);
     }
+    navigate("/room");
 
-    // Handeling the Email change
-    const handleEmail = (e) => {
-        setEmail(e.target.value);
-        setSubmitted(false);
+    if(password !== Regestration.ConfirmPassword) {
+        toast.error("Password don't match")
+    }else{
+        // make API call
     }
+  };
 
-    // Handeling the Password Change
-        const handlePassword = (e) => {
-            setPasssword(e.target.value)
-            setSubmitted(false)
-        }
-
-    // Handel Submit is login
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        if(email === "" || password === "") {
-            setError(true);
-        }else{
-            setSubmitted(true)
-            setError(false)
-        }
-        navigate("/room")
-
-        // if(password !== Regestration.ConfirmPassword) {
-        //     toast.error("Password don't match")
-        // }else{
-        //     // make API call
-        // }
-    }
-
-const errorMessage = () => {
+  const errorMessage = () => {
     return (
-        <div className="error"
-          style={{
-            display: error ? "" : "none",
-          }}>
-        <h6 style={{background: "#f79d9d"}} className="text-danger text-center py-2 px-3 rounded fw-bold">Please Enter All the Field</h6>
-        </div>
-    )
-}  
+      <div
+        className="error"
+        style={{
+          display: error ? "" : "none",
+        }}
+      >
+        <h6
+          style={{ background: "#f79d9d" }}
+          className="text-danger text-center py-2 px-3 rounded fw-bold"
+        >
+          Please Enter All the Field
+        </h6>
+      </div>
+    );
+  };
 
-const successMessage = () => {
+  const successMessage = () => {
     return (
-        <div className="success"
-         style={{
-            display: submitted ? "" : "none"
-         }}>
-            <h6 style={{background: "#5fff8f"}} className="text-success text-center rounded py-2 px-3 rounded fw-bold">Login Successfully</h6>
-         </div>
-    )
-}
+      <div
+        className="success"
+        style={{
+          display: submitted ? "" : "none",
+        }}
+      >
+        <h6
+          style={{ background: "#5fff8f" }}
+          className="text-success text-center rounded py-2 px-3 rounded fw-bold"
+        >
+          Login Successfully
+        </h6>
+      </div>
+    );
+  };
 
-const myStyle = {
+  const myStyle = {
     width: "34.3rem",
     height: "4rem",
     body: "center",
     // fontFamily: 'text new roman'
-}
-const myFont = {
-    fontFamily: 'serif'
-   }
- return(
-     <div className="container-fluid">
-        {/* <div className="col-md-4 col-sm-12">
+  };
+  const myFont = {
+    fontFamily: "serif",
+  };
+  return ( 
+    <div className="container-fluid">
+      {/* <div className="col-md-4 col-sm-12">
             1 of 1
         </div> */}
-        <div className="col-md-12 col-sm-12">
-            <div className="col-md-8 col-sm-12 login">
-              
+      <div className="col-md-12 col-sm-12">
+        <div className="col-md-8 col-sm-12 login"></div>
+        <div className="col-md-4 col-sm-12 shadow h-100">
+         <div className="row ">
+         <div className="row ms-2 py-2 h-100 w-100">
+            <div className="message">
+              {errorMessage()}
+              {successMessage()}
             </div>
-            <div className="col-md-4 col-sm-12 shadow">
-            <div className="row ms-2 py-2 h-100">
-                  <div className="message">
-                    {errorMessage()}
-                    {successMessage()}
-                  </div>
-                   <h6 className="text-center">
-                    <img className="img text-center" src={require("../assets/images/user.png")} alt=""/>
-                   </h6>
-                  {/* <h6 style={{background:'#b078fa', width:'100%' }} style={{background:'#cfe7f5'}} className="fs-3 w-100 fw-bold text-light text-center py-2 rounded shadow">Login Page</h6> */}
-                <div className="formplane row">
-                <form className=" rounded">
-                  <table>
-                    <thead>
-                        <th>
-                    <tr>
-                  <label style={myFont} className="text-bold mx-2 fs-4" htmlFor="email">Email</label>
-                  <input style={myStyle} className="input form-control" type="text" placeholder="Enter Email" name="email" id="email" onChange={(handleEmail)}/>
-                  </tr>
-                  <tr>
-                  <label style={myFont} className="text-bold mx-2 fs-4" htmlFor="psw">Password</label>
-                  <input style={myStyle} className="input form-control" type="password" placeholder="Enter Password" name="psw" id="psw" onChange={handlePassword}/>
-                  </tr>
-                  </th>
-                    </thead>
-                  </table>
-                  <button style={{background:'#7afcb0'}} className="btn mt-3 my-1 fs-6 w-100 text-bold text-success" onClick={handleSubmit}>Login</button>
-                  <button style={{background:'#91b9f5'}}className="btn text-primary fs-6 mb-3 text-bold  w-100" onClick={() => navigate("/registration")}>Registration</button>
-                 <ToastContainer/>
-                  </form>
-                 
-                </div>
-               </div>
+            <h6 className="text-center">
+              <img
+                className="img text-center"
+                src={require("../assets/images/user.png")}
+                alt=""
+              />
+            </h6>
+            {/* <h6 style={{background:'#b078fa', width:'100%' }} style={{background:'#cfe7f5'}} className="fs-3 w-100 fw-bold text-light text-center py-2 rounded shadow">Login Page</h6> */}
+            <div className="formplane row">
+              <form className=" rounded">
+                <table>
+                  <thead>
+                    <th>
+                      <tr>
+                        <label
+                          style={myFont}
+                          className="text-bold mx-2 fs-4"
+                          htmlFor="email"
+                        >
+                          Email
+                        </label>
+                        <input
+                          style={myStyle}
+                          className="input form-control"
+                          type="text"
+                          placeholder="Enter Email"
+                          name="email"
+                          id="email"
+                          onChange={handleEmail}
+                        />
+                      </tr>
+                      <tr>
+                        <label
+                          style={myFont}
+                          className="text-bold mx-2 fs-4"
+                          htmlFor="psw"
+                        >
+                          Password
+                        </label>
+                        <input
+                          style={myStyle}
+                          className="input form-control"
+                          type="password"
+                          placeholder="Enter Password"
+                          name="psw"
+                          id="psw"
+                          onChange={handlePassword}
+                        />
+                      </tr>
+                    </th>
+                  </thead>
+                </table>
+                <button
+                  style={{ background: "#7afcb0" }}
+                  className="btn mt-3 my-1 fs-6 w-100 text-bold text-light"
+                  onClick={handleSubmit}
+                >
+                  Login
+                </button>
+                <button
+                  style={{ background: "#91b9f5" }}
+                  className="btn text-light fs-6 mb-3 text-bold  w-100"
+                  onClick={() => navigate("/registration")}
+                >
+                  Registration
+                </button>
+                <ToastContainer />
+              </form>
             </div>
-            {/* <Regestration/> */}
+          </div>
+          </div> 
         </div>
-     </div>
- )
-}
-export default Login
+        {/* <Regestration/> */}
+      </div>
+    </div>
+  );
+};
+export default Login;
 
 /*
 import React, { useState } from "react"
