12ea8f9493b6095e2f06b7a06ee7a020bd0a278b
Mantosh <kyamantoshkumar@gmail.com>
2023-01-09 12:37:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Login from "./pages/Login";
 import Registration from "./pages/Registration";
 import Room from "./pages/Room";
 import Counter from "./Search/effect";
+import Mantosh from "./Search/mantosh";
 import GeeksforGeeks from "./Search/toast";
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path="/geeksforgeeks" element={<GeeksforGeeks/>} />
         <Route path ="/counter" element={<Counter/>} />
         <Route path="registrationview" element={<RegistrationView/>} />
+        <Route path ="/mantosh" element={<Mantosh/>} />
        </Routes>
       </BrowserRouter>
     </div>
